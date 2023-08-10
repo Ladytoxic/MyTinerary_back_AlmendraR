65db728e1c7fd04889a3b7c7cfb6f5eb9b9480ac
Almendra Romina R <almendraromina1@gmail.com>
2023-08-10 16:22:35 -0300

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import indexRouter from './src/router/index.router.js'
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/api', indexRouter);
+
+app.listen(PORT, () => console.log('Server running on port:', PORT));
