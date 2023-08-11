a5f86d59f86de1f252c6f8b48af4c07681194a7b
Almendra Romina R <almendraromina1@gmail.com>
2023-08-11 10:12:20 -0300

    Controller.getCities

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import indexRouter from './router/index.router.js'
+import indexRouter from './router/index.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors());
 
-app.use('/api', indexRouter);
+app.use('/', indexRouter);
 
 app.listen(PORT, () => console.log('Server running on port:', PORT));
